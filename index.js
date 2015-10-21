70099d97ac9791264238e5f2c6a2d25db6e0f100
Jose-Luis Rivas <me@ghostbar.co>
2015-10-21 16:07:18 -0500

    reformat index.js

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-'use strict';
-
-var rethink = require('rethinkdb');
+var rethink = require('rethinkdb')
 
 exports.register = function (plugin, opts, next) {
-  opts = opts || {};
+  opts = opts || {}
   if (!opts.url) {
-    opts.port = opts.port || 28015;
-    opts.host = opts.host || 'localhost';
-    opts.db = opts.db || 'test';
+    opts.port = opts.port || 28015
+    opts.host = opts.host || 'localhost'
+    opts.db = opts.db || 'test'
   } else {
-    var url = require('url').parse(opts.url);
-    opts.port = parseInt(url.port) || 28015;
-    opts.host = url.hostname || 'localhost';
-    opts.db = url.pathname ? url.pathname.replace(/^\//, '') : 'test';
+    var url = require('url').parse(opts.url)
+    opts.port = parseInt(url.port, 10) || 28015
+    opts.host = url.hostname || 'localhost'
+    opts.db = url.pathname ? url.pathname.replace(/^\//, '') : 'test'
 
     if (url.auth)
-      opts.authKey = url.auth.split(':')[1];
+      opts.authKey = url.auth.split(':')[1]
   }
 
   rethink.connect(opts, function (err, conn) {
     if (err) {
-      plugin.log(['hapi-rethinkdb', 'error'], err.message);
-      console.error(err);
-      return next(err);
+      plugin.log(['hapi-rethinkdb', 'error'], err.message)
+      console.error(err)
+      return next(err)
     }
 
-    plugin.expose('connection', conn);
-    plugin.expose('library', rethink);
-    plugin.expose('rethinkdb', rethink);
+    plugin.expose('connection', conn)
+    plugin.expose('library', rethink)
+    plugin.expose('rethinkdb', rethink)
     plugin.bind({
       rethinkdbConn: conn,
       rethinkdb: rethink
-    });
-    
-    plugin.log(['hapi-rethinkdb', 'info'], 'RethinkDB connection established');
-    return next();
-  });
-};
+    })
+
+    plugin.log(['hapi-rethinkdb', 'info'], 'RethinkDB connection established')
+    return next()
+  })
+}
 
 exports.register.attributes = {
   pkg: require('./package.json')
-};
+}
